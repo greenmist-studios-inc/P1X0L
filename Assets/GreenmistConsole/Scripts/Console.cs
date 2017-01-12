14aa12aa4de83c58428b158c44775762d14cb43a
Geoff Powell <greenmist.studios.inc@gmail.com>
2017-01-11 18:48:56 -0600

    Initial Push

---
+++ Assets/GreenmistConsole/Scripts/Console.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+namespace Greenmist {
+	
+	public class Console : MonoBehaviour {
+
+		[SerializeField]
+		protected int fps = 30;
+		private Subject<string> logSubject = new Subject<string>();
+
+		// Use this for initialization
+		void Start () {
+			
+		}
+
+		// Update is called once per frame
+		void Update () {
+			
+		}
+
+		public void Log(string message) {
+			Debug.Log (message);
+			logSubject.OnNext (message);
+		}
+
+		public void subscribe(IObserver<string> observer) {
+			logSubject.Subscribe (observer);
+		}
+	}
+}
